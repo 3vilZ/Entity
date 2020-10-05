ba2b3059db266fb548eaf4ad7e7e1cc7d6421553
3vilZ <55482184+3vilZ@users.noreply.github.com>
2020-10-05 15:01:44 +0200

    enemys

--- Identity/Assets/Scripts/Ball.cs
+++ Identity/Assets/Scripts/Ball.cs
@@ -4,11 +4,33 @@ using UnityEngine;
 
 public class Ball : MonoBehaviour
 {
+    Rigidbody2D rbBall;
+    Vector3 v3BallSpeed;
+    [SerializeField] int iDamage;
+
+    private void Start()
+    {
+        rbBall = GetComponent<Rigidbody2D>();
+    }
+
+    
+    private void Update()
+    {
+        /*
+        v3BallSpeed = rbBall.velocity;
+        v3BallSpeed.Normalize();
+        print(v3BallSpeed);
+        */
+    }
     private void OnCollisionEnter2D(Collision2D other)
     {
         if (other.gameObject.tag == "Player")
         {
             other.gameObject.GetComponent<PlayerMovement>().CatchBall();
+        }
+        else if(other.gameObject.tag == "Enemy")
+        {
+            other.gameObject.GetComponent<EnemyBase>().GetDamage(iDamage);
         }
     }
 }
---
+++ Identity/Assets/Scripts/EnemyBase.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyBase : MonoBehaviour
+{
+    
+    [SerializeField] int iMaxHealth;
+    [SerializeField] bool bMove;
+    /*
+    [SerializeField] GameObject goModel;
+    [SerializeField] float fSpeed;
+    [SerializeField] Transform tPointA;
+    [SerializeField] Transform tPointB;
+    */
+    
+    int iCurrentHealth;
+    Transform tDestiny;
+
+    void Start()
+    {
+        iCurrentHealth = iMaxHealth;
+        //tDestiny = tPointA;
+        if (!bMove)
+        {
+            GetComponent<Animator>().enabled = false;
+        }
+    }
+
+
+    void Update()
+    {
+        
+
+        /*
+        if(bMove)
+        {
+            Vector3 v3Direction = tDestiny.position - goModel.transform.position;
+            v3Direction.Normalize();
+            goModel.transform.position += v3Direction * fSpeed * Time.deltaTime;
+
+            Vector3 v3Distance = tDestiny.position - goModel.transform.position;
+            
+            if (v3Distance.x <= 0.2)
+            {
+                if(tDestiny == tPointA)
+                {
+                    tDestiny = tPointB;
+                }
+                else
+                {
+                    tDestiny = tPointA;
+                }
+            }
+        }
+        */
+    }
+
+    public void GetDamage(int fDamage)
+    {
+        iCurrentHealth -= fDamage;
+
+        if (iCurrentHealth <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+}

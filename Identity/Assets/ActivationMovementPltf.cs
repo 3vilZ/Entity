9fb4dfd585ab702ac7202b60d540d3967f011bda
PedroRibot <pedro.ribot09@gmail.com>
2020-11-04 14:23:03 +0100

    New changes scene for tutorial level

---
+++ Identity/Assets/ActivationMovementPltf.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActivationMovementPltf : MonoBehaviour
+{
+
+    [SerializeField]MovingPlatform MPcmp;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        transform.Find("B").transform.localScale = new Vector3(1, 0.3f, 1);
+        MPcmp.ActivatePltf();
+    }
+
+    
+}
---
+++ Identity/Assets/Pedrop/Scripts/MovingPlatform.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingPlatform : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    [SerializeField]GameObject Pltf;
+
+    [SerializeField]GameObject Uno;
+    [SerializeField]GameObject Dos;
+
+    [SerializeField] [Range(0, 2)] float speed;
+    [SerializeField]bool loop;
+    [SerializeField]bool activation;
+    
+    [SerializeField]GameObject activButton;
+    bool stop = false;
+    bool activated;
+    Vector3 Direcc;
+
+
+    void Start()
+    {
+        Pltf.transform.position = Uno.transform.position;
+        Direcc = Dos.transform.position - Uno.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {   
+        if (activation){    
+            if (activated){
+                Pltf.transform.position += Direcc * Time.deltaTime * speed;
+            }
+        }else{
+            if (!stop){
+                Pltf.transform.position += Direcc * Time.deltaTime * speed;
+            }
+        }
+        
+
+        if (loop){
+            if (Vector3.Distance(Pltf.transform.position, Dos.transform.position) <= 0.2f){
+                Direcc = Uno.transform.position - Dos.transform.position;
+            }else if (Vector3.Distance(Pltf.transform.position, Uno.transform.position) <= 0.2f ){
+                Direcc = Dos.transform.position - Uno.transform.position;
+            }
+        }else{
+            if (Vector3.Distance(Pltf.transform.position, Dos.transform.position) <= 0.2f){
+                stop = true;
+            }
+        }
+        
+    }
+
+    public void ActivatePltf(){
+        activated = true;
+    }
+}

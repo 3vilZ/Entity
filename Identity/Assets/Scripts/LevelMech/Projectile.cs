84c85765e77ff3cc8281b35482b76b9d2d3afdd5
3vilZ <55482184+3vilZ@users.noreply.github.com>
2020-11-21 02:48:24 +0100

    Projectile

---
+++ Identity/Assets/Scripts/LevelMech/Projectile.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+    [SerializeField] public float fBulletSpeed;
+    [SerializeField] float fBulletCd;
+    [SerializeField] Transform tAttackPos;
+    [SerializeField] GameObject goBullet;
+    [SerializeField] GameObject goCarcaj;
+    Vector3 v3BulletDirection;
+    float fPlayerDistance;
+    float fbulletCdControl = 0;
+
+
+    void Start()
+    {
+        
+        v3BulletDirection = tAttackPos.position - transform.position;
+        v3BulletDirection.Normalize();
+        goBullet.GetComponent<ProjectileBullet>().v3Direction = v3BulletDirection;
+        goBullet.GetComponent<ProjectileBullet>().fSpeed = fBulletSpeed;
+    }
+
+
+    void Update()
+    {
+        fPlayerDistance = Vector3.Distance(GameManager.Instance.GoPlayer.transform.position, transform.position);
+
+        if(fPlayerDistance <= 30)
+        {
+            fbulletCdControl -= Time.deltaTime;
+
+            if(fbulletCdControl <= 0)
+            {
+                Instantiate(goBullet, tAttackPos.position, tAttackPos.rotation, goCarcaj.transform);
+                fbulletCdControl = fBulletCd;
+            }
+        }
+
+    }
+}
---
+++ Identity/Assets/Scripts/LevelMech/ProjectileBullet.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProjectileBullet : MonoBehaviour
+{
+    [HideInInspector] public float fSpeed;
+    [HideInInspector] public Vector3 v3Direction;
+
+    void Update()
+    {
+        transform.position += v3Direction * fSpeed * Time.deltaTime;
+    }
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        Destroy(gameObject);
+    }
+}

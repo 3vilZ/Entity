af4debfd3d67b594feb6b0c15e6cbcda9c71842f
PedroRibot <pedro.ribot09@gmail.com>
2020-11-27 18:25:54 +0100

    nivel 2 + hide portion

---
+++ Identity/Assets/HideLevel.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HideLevel : MonoBehaviour
+{
+
+    public float[] colors;
+
+    void Start(){
+
+        colors = new float[transform.childCount];
+
+       int children = transform.childCount;
+
+            for (int i = 0; i < children; ++i){
+
+                Color tmp = transform.GetChild(i).GetComponent<SpriteRenderer>().color;
+                colors[i] = tmp.a;
+            }
+    }
+
+
+   void OnTriggerEnter2D(Collider2D col)
+    {
+
+
+        if (col.tag == "Player"){
+            int children = transform.childCount;
+
+            for (int i = 0; i < children; ++i){
+
+                Color tmp = transform.GetChild(i).GetComponent<SpriteRenderer>().color;
+                tmp.a = 0f;
+                transform.GetChild(i).GetComponent<SpriteRenderer>().color = tmp;
+
+            }
+        }
+        
+             
+    }
+
+    void OnTriggerExit2D(Collider2D col)
+    {
+
+        if (col.tag == "Player"){
+            int children = transform.childCount;
+            for (int i = 0; i < children; ++i){
+            
+
+            Color tmp = transform.GetChild(i).GetComponent<SpriteRenderer>().color;
+            tmp.a = colors[i];
+            transform.GetChild(i).GetComponent<SpriteRenderer>().color = tmp;
+
+            }
+        }
+    }
+}

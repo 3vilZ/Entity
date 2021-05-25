327eb80fbac9141e6b1c8b9c06c9b79d87e1e222
3vilZ <55482184+3vilZ@users.noreply.github.com>
2021-05-25 05:23:56 +0200

    final push con 0 bugs xD lmao

--- Identity/Assets/Scripts/Narrative/Godess.cs
+++ Identity/Assets/Scripts/Narrative/Godess.cs
@@ -11,16 +11,44 @@ public class Godess : MonoBehaviour
     [HideInInspector] public bool bEvent = false;
     bool bStart = false;
 
+    AudioSource asAbility;
+    float asAbilityVolume;
+    bool bAbilityStart = false;
+    bool bAbilityEnd = false;
+
 
     void Start()
     {
-        
+        asAbility = AudioManager.Instance.asAbility;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(bEvent)
+
+        if (bAbilityStart)
+        {
+            if (!asAbility.isPlaying && !bAbilityEnd)
+            {
+                asAbility.Play();
+            }
+        }
+        else
+        {
+            if (Vector2.Distance(GameManager.Instance.GoPlayer.transform.position, transform.position) <= 45)
+            {
+                //Audio
+                asAbilityVolume = asAbility.volume;
+                asAbility.volume = 0;
+                asAbility.Play();
+                StartCoroutine(AudioManager.Instance.FadeSound(asAbility, 2, asAbilityVolume));
+                AudioManager.Instance.FadeOut();
+                //Audio
+                bAbilityStart = true;
+            }
+        }
+
+        if (bEvent)
         {
             GameManager.Instance.GoPlayer.GetComponent<PlayerAnim>().Interact();
             GameManager.Instance.ScriptPlayer.bInteracting = true;
@@ -40,12 +68,19 @@ public class Godess : MonoBehaviour
             GameManager.Instance.ScriptPlayer.bInteracting = true;
             GameManager.Instance.GoPlayer.GetComponent<Rigidbody2D>().velocity = new Vector2(0, GameManager.Instance.GoPlayer.GetComponent<Rigidbody2D>().velocity.y);
 
-            goBallSlot.transform.position = Vector3.MoveTowards(goBallSlot.transform.position, goDestiny.transform.position, 0.2f);
+            goBallSlot.transform.position = Vector3.MoveTowards(goBallSlot.transform.position, goDestiny.transform.position, 0.1f);
 
             if (Vector3.Distance(goBallSlot.transform.position, goDestiny.transform.position) <= 0.1f)
             {
                 Particles();
                 StartCoroutine(EndEndEnd());
+
+                //Audio
+                StartCoroutine(AudioManager.Instance.FadeSound(asAbility, 2, 0, asAbilityVolume));
+                AudioManager.Instance.FadeIn();
+                bAbilityEnd = true;
+                //Audio
+
                 bStart = false;
             }
         }
